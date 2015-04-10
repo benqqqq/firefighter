2cc36ae2510128f6d2083ace54d099f280c0a5b9
benq <benq7879@gmail.com>
2015-04-10 11:25:23 +0800

    bug fix

--- app/views/order/layout.blade.php
+++ app/views/order/layout.blade.php
@@ -6,7 +6,7 @@
     	<title>訂餐 - 基隆消防信二分隊</title>
     	
     	<script>
-            var host = URL::to('/');
+            var host = '{{ URL::to("/") }}';
         </script>
 		{{ HTML::script('http://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular.min.js') }}
 		{{ HTML::script('http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js') }}
